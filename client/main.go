60d0fb797c6a8b9680fdbdf1066bafa1363e0172
Yutan0423 <yuutan3240@gmail.com>
2023-06-15 09:29:42 +0900

    initial commit

---
+++ client/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go-proto/generated/pb"
+	"io"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	fileServiceClient := pb.NewFileServiceClient(conn)
+	callListFiles(fileServiceClient)
+	callDownload(fileServiceClient)
+}
+
+func callListFiles(c pb.FileServiceClient) {
+	res, err := c.ListFiles(context.Background(), &pb.ListFilesRequest{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(res.GetFilenames())
+}
+
+func callDownload(c pb.FileServiceClient) {
+	req := &pb.DownloadRequest{Filename: "name.txt"}
+	stream, err := c.Download(context.Background(), req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("Response from Download(bytes): %v", res.GetData())
+		log.Printf("Response from Download(string): %v", string(res.GetData()))
+	}
+}
